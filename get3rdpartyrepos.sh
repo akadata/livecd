7c6c8c2343e19dfdfffcf8651755d4557267d1fb
root <root@localhost.localdomain>
2017-11-08 00:53:17 +0000

    Try again

---
+++ get3rdpartyrepos.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# add and pull down third party repos and generate repodata files
+
+mkdir -p /opt/rcn/rpms
+cat << EOF > /etc/yum.repos.d/prometheus.repo
+[prometheus-rpm_release]
+name=prometheus-rpm_release
+baseurl=https://packagecloud.io/prometheus-rpm/release/el/7/\$basearch/
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/prometheus-rpm/release/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+
+[prometheus-rpm_release-source]
+name=prometheus-rpm_release-source
+baseurl=https://packagecloud.io/prometheus-rpm/release/el/7/SRPMS
+repo_gpgcheck=1
+gpgcheck=0
+enabled=0
+gpgkey=https://packagecloud.io/prometheus-rpm/release/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+EOF
+
+# Import RPM keys
+rpmkeys --import https://packagecloud.io/prometheus-rpm/release/gpgkey
+yum -y update
+reposync -n -r prometheus-rpm_release -p /opt/rcn/rpms/
+createrepo /opt/rcn
