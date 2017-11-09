98b33eb5491a03dd6d00edc7b7f3b2f9ea936cc3
Scott Syms <scottsyms@gmail.com>
2017-11-09 00:37:49 +0000

    clean up package names

--- scripts/mkiso.sh
+++ scripts/mkiso.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-yum list installed|cut -d' ' -f1 | grep -v Installed|grep -v Loaded > /opt/rcn/scratch/packages.txt
-cat /opt/rcn/ks/one.ks /opt/rcn/scratch/packages.txt /opt/rch/ks/two.ks > /opt/rcn/ks/config.ks
-livecd-creator --verbose --config=/opt/rcn/ks/config.ks --fslabel=NetMonInstaller --cache=/var/cache/live
+livecd-creator --verbose --config=/opt/rcn/ks/base.ks --fslabel=NetMonInstaller --cache=/var/cache/live
